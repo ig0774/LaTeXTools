74213dd77ee4c5e496372a4dad12f3caf89361eb
Ian <ig0774@gmail.com>
2015-06-14 22:55:48 +0100

    Adapt to move of is_tex_file and get_tex_extensions

--- latex_cwl_completions.py
+++ latex_cwl_completions.py
@@ -15,14 +15,14 @@ if sublime.version() < '3000':
     from latex_ref_completions import OLD_STYLE_REF_REGEX, NEW_STYLE_REF_REGEX
     from getRegion import get_Region
     from getTeXRoot import get_tex_root
-    from latextools_utils.is_tex_file import is_tex_file, get_tex_extensions
+    from latextools_utils import is_tex_file, get_tex_extensions
 else:
     _ST3 = True
     from .latex_cite_completions import OLD_STYLE_CITE_REGEX, NEW_STYLE_CITE_REGEX, match
     from .latex_ref_completions import OLD_STYLE_REF_REGEX, NEW_STYLE_REF_REGEX
     from .getRegion import get_Region
     from .getTeXRoot import get_tex_root
-    from .latextools_utils.is_tex_file import is_tex_file, get_tex_extensions
+    from .latextools_utils import is_tex_file, get_tex_extensions
 
 # Do not do completions in these envrioments
 ENV_DONOT_AUTO_COM = [
