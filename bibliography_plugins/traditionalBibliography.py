eebbc30f62ee181c982066dc4ec6e25cd9dd0728
Ian <ig0774@gmail.com>
2015-11-25 16:26:23 +0000

    Fix the code to find the bibliography file rather than just the line

--- bibliography_plugins/traditionalBibliography.py
+++ bibliography_plugins/traditionalBibliography.py
@@ -158,11 +158,10 @@ class TraditionalBibliographyPlugin(LaTeXToolsPlugin):
 
         for content in walk_subfiles(rootdir, root_file):
             bibtags =  re.findall(r'\\bibliography\{([^\}]+)\}', content)
-            bibtags += re.findall(r'\\addbibresource(?:\[[^\]]*\])?\{[^\}]+\.bib\}', content)
-            bibtags += re.findall(r'\\addglobalbib(?:\[[^\]]*\])?\{[^\}]+\.bib\}', content)
-            bibtags += re.findall(r'\\addsectionbib(?:\[[^\]]*\])?\{[^\}]+\.bib\}', content)
+            bibtags += re.findall(r'\\addbibresource(?:\[[^\]]*\])?\{([^\}]+)\.bib\}', content)
+            bibtags += re.findall(r'\\addglobalbib(?:\[[^\]]*\])?\{([^\}]+)\.bib\}', content)
+            bibtags += re.findall(r'\\addsectionbib(?:\[[^\]]*\])?\{([^\}]+)\.bib\}', content)
             bibtags += re.findall(r'\\begin\{refsection\}\[([^\]]+)\]', content)
-            bibtags += re.findall(r'\\bibliography\{([^\}]+)\}', content)
 
             # extract absolute filepath for each bib file
             for tag in bibtags:
