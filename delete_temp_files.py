5b1c5e59cbcdcd62d580d57b71adbe362f74fa83
Ian <ig0774@gmail.com>
2016-04-03 15:18:57 +0100

    Add support for --output-directory and --aux-directory

--- delete_temp_files.py
+++ delete_temp_files.py
@@ -5,16 +5,21 @@ if sublime.version() < '3000':
 	_ST3 = False
 	# we are on ST2 and Python 2.X
 	import getTeXRoot
-
 	from latextools_utils import cache, get_setting
+	from latextools_utils.output_directory import (
+		get_aux_directory, get_output_directory
+	)
 else:
 	_ST3 = True
 	from . import getTeXRoot
 	from .latextools_utils import cache, get_setting
-
+	from .latextools_utils.output_directory import (
+		get_aux_directory, get_output_directory
+	)
 
 import sublime_plugin
 import os
+import shutil
 
 import traceback
 
@@ -56,33 +61,75 @@ class DeleteTempFilesCommand(sublime_plugin.WindowCommand):
 			print('Error while trying to delete local cache')
 			traceback.print_exc()
 
-		path = os.path.dirname(root_file)
+		aux_directory = get_aux_directory(root_file)
+		output_directory = get_output_directory(root_file)
 
-		# Load the files to delete from the settings
-		temp_files_exts = get_setting('temp_files_exts',
-			['.blg', '.bbl', '.aux', '.log', '.brf', '.nlo', '.out', '.dvi',
-			 '.ps', '.lof', '.toc', '.fls', '.fdb_latexmk', '.pdfsync',
-			 '.synctex.gz', '.ind', '.ilg', '.idx'])
+		if aux_directory is not None:
+			# we cannot delete the output directory on Windows in case
+			# Sumatra is holding a reference to it
+			if (
+				sublime.platform() != 'windows' or
+				aux_directory != output_directory
+			):
+				self._rmtree(aux_directory)
 
-		ignored_folders = get_setting('temp_files_ignored_folders',
-			['.git', '.svn', '.hg'])
-		ignored_folders = set(ignored_folders)
+		if output_directory is not None:
+			# we cannot delete the output directory on Windows in case
+			# Sumatra is holding a reference to it
+			if sublime.platform() == 'windows':
+				self._clear_dir(output_directory)
+			else:
+				self._rmtree(aux_directory)
+		else:
+			# if there is no output directory, we may need to clean files
+			# in the main directory, even if aux_directory is used
+			path = os.path.dirname(root_file)
 
-		for dir_path, dir_names, file_names in os.walk(path):
-			dir_names[:] = [d for d in dir_names if d not in ignored_folders]
-			for file_name in file_names:
-				for ext in temp_files_exts:
-					if file_name.endswith(ext):
-						file_name_to_del = os.path.join(dir_path, file_name)
-						if os.path.exists(file_name_to_del):
-							try:
-								os.remove(file_name_to_del)
-							except OSError:
-								# basically here for locked files in Windows,
-								# but who knows what we might find?
-								print('Error while trying to delete {0}'.format(file_name_to_del))
-								traceback.print_exc()
-						# exit extension
-						break
+			# Load the files to delete from the settings
+			temp_files_exts = get_setting('temp_files_exts',
+				['.blg', '.bbl', '.aux', '.log', '.brf', '.nlo', '.out', '.dvi',
+				 '.ps', '.lof', '.toc', '.fls', '.fdb_latexmk', '.pdfsync',
+				 '.synctex.gz', '.ind', '.ilg', '.idx'])
+
+			ignored_folders = get_setting('temp_files_ignored_folders',
+				['.git', '.svn', '.hg'])
+
+			ignored_folders = set(ignored_folders)
+
+			for dir_path, dir_names, file_names in os.walk(path):
+				dir_names[:] = [d for d in dir_names if d not in ignored_folders]
+				for file_name in file_names:
+					for ext in temp_files_exts:
+						if file_name.endswith(ext):
+							self._rmfile(os.path.join(dir_path, file_name))
+							# exit extension
+							break
 
 		sublime.status_message("Deleted temp files")
+
+	def _rmtree(self, path):
+		if os.path.exists(path):
+			try:
+				shutil.rmtree(path)
+			except OSError:
+				if os.path.exists(path):
+					# report the exception if the folder didn't end up deleted
+					traceback.print_exc()
+
+	def _rmfile(self, path):
+		if os.path.exists(path):
+			try:
+				os.remove(path)
+			except OSError:
+				if os.path.exists(path):
+					# basically here for locked files in Windows,
+					# but who knows what we might find?
+					print('Error while trying to delete {0}'.format(path))
+					traceback.print_exc()
+
+	def _clear_dir(self, path):
+		for root, directories, file_names in os.walk(path):
+			for directory in directories:
+				self._rmtree(os.path.join(root, directory))
+			for file_name in file_names:
+				self._rmfile(os.path.join(root, file_name))
