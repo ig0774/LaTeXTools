c693f0d66af79f8d7b6ac0581fbcf89c04146d15
Ian <ig0774@gmail.com>
2016-05-17 22:07:11 +0100

    Update README.pdf

--- readme/build-readme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
-chmod +x $DIR/readme-filter.py
-pandoc -f markdown_github+yaml_metadata_block -H $DIR/header-include.tex -V colorlinks=true -F $DIR/readme-filter.py -o $DIR/../README.pdf --listings $DIR/../README.markdown $DIR/metadata.yaml
