7f06e8be0e16ae766d209f263a562bfc33d0b61e
Ian <ig0774@gmail.com>
2016-07-14 12:49:06 +0100

    Unify command completions with better support for braces
    This is a rewrite of the LatexFillAllCommand to add a better bracket-matching algorithm and generally make it simpler to implement similar behaviour across our auto-completing commands.
    ## To make a commit, type your commit message and press SUPER-ENTER. To cancel
    ## the commit, close the window. To sign off on the commit, press SUPER-S.
    
    ## You may also reference or close a GitHub issue with this commit.  To do so,
    ## type `#` followed by the `tab` key.  You will be shown a list of issues
    ## related to the current repo.  You may also type `owner/repo#` plus the `tab`
    ## key to reference an issue in a different GitHub repo.

--- bibliography_plugins/traditionalBibliography.py
+++ bibliography_plugins/traditionalBibliography.py
@@ -128,6 +128,3 @@ class TraditionalBibliographyPlugin(LaTeXToolsPlugin):
 
             print("Found %d total bib entries" % (len(entries),))
         return entries
-
-    def on_insert_citation(self, keyword):
-        print('Inserted {0}'.format(keyword))
